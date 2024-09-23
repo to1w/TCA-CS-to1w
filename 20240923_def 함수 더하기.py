0ba02b182bbdcfc4dfebd33e3b5ebebe9b958490
to1w <sophiakim.051505@gmail.com>
2024-09-23 17:35:41 +0900

    Add files via upload

---
+++ 20240923_def 함수 더하기.py
@@ -0,0 +1,5 @@
+def add(a,b):
+    return a+b
+result = add(5,39)
+
+print(result)
---
+++ 20240923_def 함수 이름.py
@@ -0,0 +1,4 @@
+def name(name="friend"):
+    return f"I met {name} when I was going to the gym today!"
+
+print(name("Christine"))
